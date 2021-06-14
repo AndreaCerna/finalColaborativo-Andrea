dda006a836519a2df951a27ce917160b45e4eeff
AndreaCerna <katherinecerna1994@gmail.com>
2021-06-13 22:36:01 -0600

    Primera Serie del examen final

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.plantilla');
 });
+Route::get('/formulario', [\App\Http\Controllers\EstudianteController::class, 'registrar'])->name('registrar');
+Route::post('/guardar', [\App\Http\Controllers\EstudianteController::class, 'guardar'])->name('guardar');
+Route::get('/mostrar',[\App\Http\Controllers\EstudianteController::class, 'mostrar'])->name('mostrar');
+
+Route::get('/formulariogenero', [\App\Http\Controllers\GeneroController::class, 'form'])->name('registrarge');
+Route::post('/guardargenero', [\App\Http\Controllers\GeneroController::class, 'save'])->name('guardargene');
+Route::get('/mostrargenero',[\App\Http\Controllers\GeneroController::class, 'mostrarge'])->name('mostrarge');
+
+
+
